bca3924c4e77e39aef54eabb278dffc8acfa82ac
Arthur Oliveira <arthurolivsira@gmail.com>
2023-02-16 20:38:45 -0300

    Inicializando lista

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play();
+  document.querySelector('#som_tecla_pom').play()
 }
 
-function tocaSomClap() {
-  document.querySelector('#som_tecla_clap').play();
-}
+const listaDeTeclas = document.querySelectorAll('.tecla')
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+listaDeTeclas[0].onclick = tocaSomPom
