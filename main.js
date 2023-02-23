7cc5c54f1be79ab1d7caa085756f31ba325d2f4c
Arthur Oliveira <arthurolivsira@gmail.com>
2023-02-22 21:55:39 -0300

    Iterando em listas

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function tocaSomPom() {
-  document.querySelector('#som_tecla_pom').play()
+function tocaSom(idElemmentAudio) {
+  document.querySelector(idElemmentAudio).play()
 }
-
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-listaDeTeclas[0].onclick = tocaSomPom
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador]
+  const instrumento = tecla.classList[1]
+  const idAudio = `#som_${instrumento}`
+  tecla.onclick = function () {
+    tocaSom(idAudio)
+  }
+}
